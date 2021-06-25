28f7eb77aec694a0963ddb2708b08226b0841c23
Martyna Kuciara <mkuciara@o2.pl>
2021-06-25 19:15:19 +0200

    adding new task interaction

--- src/App.js
+++ src/App.js
@@ -35,7 +35,18 @@ function App() {
       ...task,
       done: true,
     })));
-  }
+  };
+
+  const addNewTask = (newTaskContent) => {
+    setTasks(tasks => [
+      ...tasks,
+      {
+        content: newTaskContent,
+        done: false,
+        id: tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1,
+      }
+    ])
+  };
 
 
   return (
@@ -43,7 +54,7 @@ function App() {
       <Header title="Lista zadań" />
       <Section
         title="Dodaj nowe zadanie"
-        body={<Form />}
+        body={<Form addNewTask={addNewTask} />}
       />
 
       <Section
